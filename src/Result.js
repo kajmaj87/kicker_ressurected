ebf9a69415d9b255e3f5654dd9042120f3a10302
ACH <aleksandra.dolega@capgemini.com>
2021-10-01 16:47:38 +0200

    approach with dropdown

--- src/Result.js
+++ src/Result.js
@@ -1,8 +1,9 @@
 import {useEffect, useState} from 'react';
 import {useHistory, useLocation} from 'react-router-dom';
 import {Tab, TabList, TabPanel, Tabs} from 'react-tabs';
+//import DropdownInput from 'react-dropdown-input';
+import React from 'react';
 import 'react-tabs/style/react-tabs.css';
-import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 import * as FirestoreService from './services/firestore';
 
@@ -11,10 +12,27 @@ const Result = (props) => {
         const history = useHistory();
         const location = useLocation();
         const [resultList, setResultList] = useState([]);
-        const [open, setOpen] = useState(false);
-        const closeModal = () => setOpen(false);
-
+        const [groupList, setGroupList] = useState([]);
+        const [options, setOptions] = useState([]);
+        const [group1users, setGroup1Users] = useState([]);
+        const [group2users, setGroup2Users] = useState([]);
         useEffect(() => {
+            const groupList = [];
+            const options = [];
+            FirestoreService.getGroupList().then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    groupList.push(doc.data().name);
+                });
+                setGroupList(groupList);
+
+                for (let i = 0; i < groupList.length; i++) {
+                    const value = groupList[i];
+                    options.push(<option key={value} value={value}>{value}</option>);
+                }
+                setOptions(options);
+            });
+
+
             const userList = [];
             FirestoreService.getRankingList(location.state.param)
                 .then((querySnapshot) => {
@@ -23,71 +41,139 @@ const Result = (props) => {
                     });
                     setResultList(userList);
                 });
+
         }, []);
+
+        // getUsers(() => {
+        //
+        // });
+
+        const getGroup1Users = (group) => {
+            const userList = [];
+            FirestoreService.getRankingList(group.value)
+                .then((querySnapshot) => {
+                    querySnapshot.forEach((doc) => {
+                        userList.push(doc.data().name);
+                    });
+                    setGroup1Users(userList);
+                });
+        };
+
+        const getGroup2Users = (group) => {
+            const userList = [];
+            FirestoreService.getRankingList(group)
+                .then((querySnapshot) => {
+                    querySnapshot.forEach((doc) => {
+                        userList.push(doc.data().name);
+                    });
+                    setGroup2Users(userList);
+                });
+        };
 
 
         return (
             <>
                 <h1>Result Page {location.state.param}</h1>
                 <br/>
-                <Popup open={open} closeOnDocumentClick onClose={closeModal}>
-                    <div className="header">Add new result</div>
-                    <div className="content">
-                        <label>
-                            Date:
-                            <input type="text" date="date"/>
-                        </label>
-                        <label>
-                            Group Win:
-                            <input type="text" groupWin="name"/>
-                        </label>
-                        <label>
-                            Loser group:
-                            <input type="text" loserGroup="name"/>
-                        </label>
-                        <button
-                            className="button"
-                            onClick={closeModal}>
-                            Submit
-                        </button>
-                        <button
-                            className="button"
-                           //onClick={FirestoreService.addMatch(["PKU", "BSZ"], ["AKO", "PLA"], "ASPEP")}>
-                            onClick={closeModal}>
-                            Submit
-                        </button>
-
-                    </div>
-                </Popup>
 
                 <Tabs>
                     <TabList>
                         <Tab>ranking</Tab>
                         <Tab>history</Tab>
-                        <button type="button" className="button" onClick={() => setOpen(o => !o)}>
-                            add result
-                        </button>
+                        <Tab>new game result</Tab>
                     </TabList>
 
                     <TabPanel>
                         <h2>TABELA Z RANKINGIEM</h2>
+                        {<ul>
+                            {
+                                resultList.map(result =>
+                                    <li>{result}</li>
+                                )
+                            }
+
+                        </ul>}
                     </TabPanel>
                     <TabPanel>
-                        <h2>TABELA Z WYNIKAMIdffdfd</h2>
+                        <h2>TABELA Z WYNIKAMI</h2>
+                    </TabPanel>
+                    <TabPanel>
+                        <h2>DODAJTA KTO WYGRAJTA</h2>
+                        <div className="header">Add new result</div>
+                        <div className="content">
+                            <label>
+                                Date:
+                            </label>
+                            <input type="text" date="date"/>
+
+                            <br/>
+                            <label>Winners: </label>
+                            <select onChange={() => getGroup1Users(this)}>
+                                {options}
+                            </select>
+
+                            <br/>
+                            <label>Winner 1: </label>
+                            <select>
+                                {group1users}
+                            </select>
+
+                            <br/>
+                            <label>Winner 2: </label>
+                            <select>
+                                {group1users}
+                            </select>
+
+                            <br/>
+                            <br/>
+                            <br/>
+                            <label>Losers: </label>
+                            <select onChange={() => getGroup2Users(this)}>
+                                {options}
+                            </select>
+
+                            <br/>
+                            <label>Loser 1: </label>
+                            <select>
+                                {group2users}
+                            </select>
+
+                            <br/>
+                            <label>Loser 2: </label>
+                            <select>
+                                {group2users}
+                            </select>
+
+                            {/*<DropdownInput*/}
+                            {/*  //  options={groupList}*/}
+                            {/*    menuClassName='dropdown-input'*/}
+                            {/*    //onSelect={this.handleSelectName}*/}
+                            {/*    placeholder='Winners'*/}
+                            {/*/>*/}
+
+                            {/*<DropdownInput*/}
+                            {/* //   options={groupList}*/}
+                            {/*    menuClassName='dropdown-input'*/}
+                            {/*    //onSelect={this.handleSelectName}*/}
+                            {/*    placeholder='Losers'*/}
+                            {/*/>*/}
+
+                            <br/>
+                            <button
+                                className="button"
+                                //onClick={FirestoreService.addMatch(["PKU", "BSZ"], ["AKO", "PLA"], "ASPEP")}
+                            >
+                                Submit
+                            </button>
+
+                        </div>
                     </TabPanel>
                 </Tabs>
 
-                {<ul>
-                    {
-                        resultList.map(result =>
-                            <li>{result}</li>
-                        )
-                    }
-
-                </ul>}
                 <button onClick={() => history.goBack()}>Go Back</button>
             </>
-        );
+        )
+            ;
     }
 ;
 
